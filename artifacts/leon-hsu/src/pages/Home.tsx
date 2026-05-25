afa601f9c59c25cd45108f5ac4fd9f206ce53a40
leonhsuin123 <leonhsuin123@gmail.com>
2026-05-25 00:10:55 -0400

    Update Home.tsx

--- artifacts/leon-hsu/src/pages/Home.tsx
+++ artifacts/leon-hsu/src/pages/Home.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         {/* Background image */}
         <div
           className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-40 z-0"
-          style={{ backgroundImage: "url('home-bg.png')" }}
+          style={{ backgroundImage: "url('/home-bg.png')" }}
         />
 
         {/* Dark atmospheric background */}
