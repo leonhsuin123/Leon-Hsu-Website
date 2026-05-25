db680b2a19862ee5604a5495adfff41f184a2499
leonhsuin123 <leonhsuin123@gmail.com>
2026-05-24 21:54:45 -0400

    Update About.tsx

--- artifacts/leon-hsu/src/pages/About.tsx
+++ artifacts/leon-hsu/src/pages/About.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import PageHeader from "@/components/PageHeader";
 import { useTranslation } from "@/contexts/TranslationContext";
